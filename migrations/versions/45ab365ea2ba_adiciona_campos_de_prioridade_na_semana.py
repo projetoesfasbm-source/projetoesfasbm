df1e7561f344b731602d88a655c00155393c6c99
projetoesfasBM <projetoesfasbm@gmail.com>
2025-12-05 19:02:09 +0000

    Atualizacoes automaticas

---
+++ migrations/versions/45ab365ea2ba_adiciona_campos_de_prioridade_na_semana.py
@@ -0,0 +1,24 @@
+"""Adiciona campos de prioridade na semana
+
+Revision ID: 45ab365ea2ba
+Revises: b7832e7624ab
+Create Date: 2025-12-05 18:11:00.437984
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '45ab365ea2ba'
+down_revision = 'b7832e7624ab'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
