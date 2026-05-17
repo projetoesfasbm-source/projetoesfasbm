6b2615edb4ff8b91a8246a067f80c7f5cb00cc2a
Seu Nome Completo <seu.email@gmail.com>
2026-05-17 20:48:10 -0300

    Merge migration branches

---
+++ migrations/versions/043654b10917_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 043654b10917
+Revises: 54f30bd67373, e0774a5d3cd0
+Create Date: 2026-05-17 23:47:44.476877
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '043654b10917'
+down_revision = ('54f30bd67373', 'e0774a5d3cd0')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
