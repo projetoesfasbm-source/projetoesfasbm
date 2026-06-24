08582a39c32b093821e959aedb0e86f2ea8f1eaf
projetoesfasbm-source <projetoesfasbm@gmail.com>
2026-06-24 16:52:35 -0300

    fix: Adiciona arquivo de migration bridge para corrigir erro de deploy no Render

---
+++ migrations/versions/6dd1cde59b0d_dummy_bridge.py
@@ -0,0 +1,24 @@
+"""dummy bridge for render
+
+Revision ID: 6dd1cde59b0d
+Revises: 087a4f39177e
+Create Date: 2026-06-24 16:55:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6dd1cde59b0d'
+down_revision = '087a4f39177e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
