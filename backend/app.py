515b342064a6e1731c55c356783470ad62f1f2b9
Dyplaytv <145167166+Dyplaytv@users.noreply.github.com>
2026-06-25 13:57:13 -0300

    fix(app): usa alunos_profiles no selectinload do load_user

--- backend/app.py
+++ backend/app.py
@@ -139,11 +139,11 @@ def create_app(config_class=Config):
 
     @login_manager.user_loader
     def load_user(user_id):
-        from sqlalchemy.orm import joinedload
+        from sqlalchemy.orm import selectinload
         return db.session.query(User).options(
-            joinedload(User.user_schools),
-            joinedload(User.aluno_profile),
-            joinedload(User.instrutor_profile)
+            selectinload(User.user_schools),
+            selectinload(User.alunos_profiles),
+            selectinload(User.instrutor_profile)
         ).get(int(user_id))
 
     # =========================================================
